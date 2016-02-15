fc4f53614a7336bb11e6b205236e444016da138e
mmmateaaa <sim.mat06@gmail.com>
2016-02-15 16:20:48 +0100

    input

--- js/main.js
+++ js/main.js
@@ -4,13 +4,14 @@ jQuery(document).ready(function($){
 		if($(event.target).is('.cd-main-nav')) $(this).children('ul').toggleClass('is-visible');
 	});
 
-	//page scroll
+	//page scroll for arrow bounce
 	$('#about-us').click(function(){
 		$('html, body').animate({
 			scrollTop: $("#more").offset().top
 		}, 1000);
 	});
 
+	//page scroll
 	$(function() {
 	    $('body').on('click', '.page-scroll a', function(event) {
 	        var $anchor = $(this);
@@ -22,6 +23,16 @@ jQuery(document).ready(function($){
 	    
 	});
 
+	// Floating label headings for the contact form
+	$(function() {
+	    $("body").on("input propertychange", ".floating-label-form-group", function(e) {
+	        $(this).toggleClass("floating-label-form-group-with-value", !! $(e.target).val());
+	    }).on("focus", ".floating-label-form-group", function() {
+	        $(this).addClass("floating-label-form-group-with-focus");
+	    }).on("blur", ".floating-label-form-group", function() {
+	        $(this).removeClass("floating-label-form-group-with-focus");
+	    });
+	});
 
 	//set animation timing
 	var animationDelay = 2500,
